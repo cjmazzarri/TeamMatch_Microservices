d85fe8121b0d96d7245d4c50bc37a1e282a659d3
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-06-06 12:15:40 -0500

    ✨H2 MatchMaking
    ✨H2 MatchMaking

--- matchmaking-service/src/main/java/com/teammatch/matchmaking/MatchmakingApplication.java
+++ matchmaking-service/src/main/java/com/teammatch/matchmaking/MatchmakingApplication.java
@@ -2,10 +2,12 @@ package com.teammatch.matchmaking;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.teammatch.matchmaking","com.teammatch.matchmaking"})
 public class MatchmakingApplication {
 
     public static void main(String[] args) {
