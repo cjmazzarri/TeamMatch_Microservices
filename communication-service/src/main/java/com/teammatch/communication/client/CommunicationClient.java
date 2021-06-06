9541c89c794666fdafc3e8be466f000d6d724245
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-06-05 21:28:58 -0500

    Added models. services, repositories and controllers :sparkles:

---
+++ communication-service/src/main/java/com/teammatch/communication/client/CommunicationClient.java
@@ -0,0 +1,10 @@
+package com.teammatch.communication.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "communication-service")
+@RequestMapping(value = "/communications")
+public interface CommunicationClient {
+    //Aqui colocar todos los metodos a utilizar
+}
