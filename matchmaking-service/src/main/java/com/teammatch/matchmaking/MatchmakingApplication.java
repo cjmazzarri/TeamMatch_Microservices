bd8c0dee2e5983f6164015f729a2dde8e78af156
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-06-21 17:43:14 -0500

    🐛Fixed Inexistent Endpoint
    🐛Fixed Inexistent Endpoint

--- matchmaking-service/src/main/java/com/teammatch/matchmaking/MatchmakingApplication.java
+++ matchmaking-service/src/main/java/com/teammatch/matchmaking/MatchmakingApplication.java
@@ -2,15 +2,11 @@ package com.teammatch.matchmaking;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-@EnableDiscoveryClient
-@EnableFeignClients
 @SpringBootApplication
 public class MatchmakingApplication {
 
