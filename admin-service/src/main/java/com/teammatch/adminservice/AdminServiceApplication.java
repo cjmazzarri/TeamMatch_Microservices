4cc1b901c597e2b80d17bfc327e4b6d0f06b5aad
Juan Alonso <leyva.calle1403@gmail.com>
2021-06-21 13:42:17 -0500

    mini change

--- admin-service/src/main/java/com/teammatch/adminservice/AdminServiceApplication.java
+++ admin-service/src/main/java/com/teammatch/adminservice/AdminServiceApplication.java
@@ -4,8 +4,9 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAdminServer
+
 @SpringBootApplication
+@EnableAdminServer
 public class AdminServiceApplication {
 
     public static void main(String[] args) {
