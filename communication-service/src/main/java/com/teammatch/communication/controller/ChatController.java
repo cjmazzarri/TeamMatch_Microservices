5dd3b774f3cae329e15be1636dc1585d7b5593b4
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-06-29 18:18:36 -0500

    🐛Fixed Microservices' Routes
    🐛Fixed Microservices' Routes

--- communication-service/src/main/java/com/teammatch/communication/controller/ChatController.java
+++ communication-service/src/main/java/com/teammatch/communication/controller/ChatController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/communication")
 @CrossOrigin
 @Tag(name = "Chats", description = "Chats API")
 public class ChatController {
